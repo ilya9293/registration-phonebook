b9bc84cbb0a184dfc6874138f1d5a4fc03e5546f
ilya9293 <ilya9293@ukr.net>
2023-01-06 16:34:47 +0200

    add form sign up

--- src/pages/Ragistration.jsx
+++ src/pages/Ragistration.jsx
@@ -1,8 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function Ragistration(props) {
-  return <div>Ragistration</div>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleChange = e => {
+    switch (e.target.name) {
+      case 'name':
+        setName(e.target.value);
+        break;
+      case 'email':
+        setEmail(e.target.value);
+        break;
+      case 'password':
+        setPassword(e.target.value);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <form className="formSignup">
+      {/* //  onSubmit={pushSubmit}> */}
+      <label>
+        Login
+        <input type="text" name="name" required value={name} onChange={handleChange} />
+      </label>
+      <label>
+        E-mail
+        <input type="email" name="email" required value={email} onChange={handleChange} />
+      </label>
+      <label>
+        Password
+        <input type="password" name="password" required value={password} onChange={handleChange} />
+      </label>
+      <button type="submit" className="submit">
+        Add contact
+      </button>
+    </form>
+  );
 }
 
 Ragistration.propTypes = {};
